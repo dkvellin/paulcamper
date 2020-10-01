c54db6ceb415c66f1589ccab2c92e913a9b9131e
Deepak Kumar Vellingiri <dkvellin@gmail.com>
2020-10-02 02:51:01 +0530

    Added Code to Test Client Bundle

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const Home = () => <div>I am rendered from Home component</div>;
+const Home = () => {
+  return (
+    <div>
+      <div>I am rendered from Home component</div>
+      <button onClick={() => console.log('Onclick was called')}>
+        Click Me!
+      </button>
+    </div>
+  );
+};
 
 export default Home;
